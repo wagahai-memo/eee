d26036c0c2e9d8d20dc00140d2c944691f3ced16
m-inata <m.inata303@gmail.com>
2020-09-06 22:54:56 +0900

    add toString()

--- Rational.java
+++ Rational.java
@@ -1,4 +1,9 @@
 public class Rational {
     private final int numerator;
     private final int denominator;
+
+    @Override
+    public String toString() {
+        return "" + numerator + "/" + denominator;
+    }
 }
